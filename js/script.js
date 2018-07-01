f573be4ffbbb5ad75d2ae49277af2a57171c8765
Zameel Zubair <zameelmalol@gmail.com>
2018-07-01 17:13:16 +0300

    Updated pics and code

--- js/script.js
+++ js/script.js
@@ -301,7 +301,7 @@ $(document).ready(function () {
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1500,
+        autoplaySpeed: 1000,
         arrows: false,
         dots: false,
         accessibility: true,
