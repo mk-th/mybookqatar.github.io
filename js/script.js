0b298958c0a209d767fa4cabd870ad3b3ba61e62
Zameel Zubair <zameelmalol@gmail.com>
2018-07-03 12:38:27 +0300

    Work Hours and About Us

--- js/script.js
+++ js/script.js
@@ -347,3 +347,32 @@ $(document).ready(function () {
         console.log("err");
     }
 });
+
+var d = new Date("July 11, 2018 13:59:00")
+
+if (d.getDay() == 4) { //Thu
+    if (d.getHours() >= 9 && d.getHours() < 15) {
+        document.getElementById("thuxx").className = "circle green";
+        document.getElementById("messageHours").innerHTML = "Yes, we're open!";
+    } else {
+        document.getElementById("thuxx").className = "circle grey";
+        document.getElementById("messageHours").innerHTML = "Sorry, we're closed!";
+    }
+    document.getElementById("frixx").className = "hide";
+    document.getElementById("restxx").className = "hide";
+} else if (d.getDay() == 5) { //Fri
+    document.getElementById("frixx").className = "circle grey";
+    document.getElementById("thuxx").className = "hide";
+    document.getElementById("restxx").className = "hide";
+    document.getElementById("messageHours").innerHTML = "Sorry, we're closed!";
+} else {
+    if (d.getHours() >= 9 && d.getHours() < 18) {
+        document.getElementById("restxx").className = "circle green";
+        document.getElementById("messageHours").innerHTML = "Yes, we're open!";
+    } else {
+        document.getElementById("restxx").className = "circle grey";
+        document.getElementById("messageHours").innerHTML = "Sorry, we're closed!";
+    }
+    document.getElementById("thuxx").className = "hide";
+    document.getElementById("frixx").className = "hide";
+}
