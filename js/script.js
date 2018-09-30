da0d5dff893af4ab94aa1a5b48b2e3df3c270ace
mybookqatar <35250807+mybookqatar@users.noreply.github.com>
2018-09-30 15:40:21 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -93,7 +93,6 @@ $(document).ready(function () {
 
 
     // Initialize Firebase
-
     var config = {
         apiKey: "AIzaSyArCQMJGfncTdxllXugahAapUH5UZqJh9I",
         authDomain: "mbqmerchstats.firebaseapp.com",
