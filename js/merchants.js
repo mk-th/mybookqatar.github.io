5be8ef88c976b5e6b6c44b6acb8aef847c82aa04
Zameel Zubair <zameelmalol@gmail.com>
2019-03-19 08:08:03 +0300

    Update merchants.js

--- js/merchants.js
+++ js/merchants.js
@@ -117,7 +117,7 @@ $(document).ready(function () {
             if (data.length == headers.length) {
 
                 if (data[4] === "Beauty & Spas") {
-                    if (data[0] === "2019 Alive, No Contract") {
+                    if (data[0] === "2019 Alive No Contract") {
                         alert(data[3]);
                     }
                     if (data[0] !== "2019 Alive On App") {
